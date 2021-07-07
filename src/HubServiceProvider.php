0883d32b8d64ffc83b75698f85f083e6f336a715
Guilherme Haynes Howe <zerossb@gmail.com>
2021-07-07 13:58:27 -0300

    Add more configurations on HubServiceProvider.php and hub.php

--- src/HubServiceProvider.php
+++ src/HubServiceProvider.php
@@ -45,9 +45,9 @@ class HubServiceProvider extends ServiceProvider
             );
         }
 
+        $this->loadRoutesFrom(__DIR__ . '../routes/api.php');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->commands([InstallHub::class]);
-
 
         $router = $this->app->make(Router::class);
         $router->aliasMiddleware('hub.auth', AuthenticateHubMiddleware::class);
