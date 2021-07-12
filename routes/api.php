44dffef5edd47917ac8ec7927ca7b4a896ec3428
Guilherme Haynes Howe <zerossb@gmail.com>
2021-07-12 13:21:56 -0300

    Ass /users/me route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use BildVitta\Hub\Http\Controllers\Auth\CallbackController;
 use BildVitta\Hub\Http\Controllers\Auth\LoginController;
 use BildVitta\Hub\Http\Controllers\Auth\LogoutController;
 use BildVitta\Hub\Http\Controllers\Auth\RefreshController;
+use BildVitta\Hub\Http\Controllers\Users\MeController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/auth')->middleware('throttle')->group(function () {
@@ -11,4 +12,8 @@ Route::prefix('api/auth')->middleware('throttle')->group(function () {
     Route::get('logout')->name('auth.logout')->uses(LogoutController::class);
     Route::get('callback')->name('auth.callback')->uses(CallbackController::class);
     Route::get('refresh')->name('auth.refresh')->uses(RefreshController::class);
+});
+
+Route::prefix('api/users/')->middleware('hub.auth')->group(function () {
+    Route::get('me')->name('users.me')->uses(MeController::class);
 });
