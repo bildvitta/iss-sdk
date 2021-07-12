e39af504dce316b305de1f33d6473084b9b20ec0
Guilherme Haynes Howe <zerossb@gmail.com>
2021-07-12 11:19:50 -0300

    Add api route on routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use BildVitta\Hub\Http\Controllers\Auth\LogoutController;
 use BildVitta\Hub\Http\Controllers\Auth\RefreshController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->middleware('throttle')->group(function () {
+Route::prefix('api/auth')->middleware('throttle')->group(function () {
     Route::get('login')->name('auth.login')->uses(LoginController::class);
     Route::get('logout')->name('auth.logout')->uses(LogoutController::class);
     Route::get('callback')->name('auth.callback')->uses(CallbackController::class);
