18a4e0bdd5cf026b428afdd3f081a7caaffb08aa
Estevão Simões <estevao.simoes@live.com>
2022-12-27 16:15:26 -0300

    adding callback route

---
+++ src/Http/Controllers/Users/ClearCacheController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace BildVitta\Hub\Http\Controllers\Users;
+
+use BildVitta\Hub\Entities\HubUser;
+use BildVitta\Hub\Http\Requests\MeRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cache;
+
+class ClearCacheController extends UsersController
+{
+    /**
+     * @param MeRequest $request
+     * @return Response
+     */
+    public function __invoke(MeRequest $request, $user): Response
+    {
+        $user = config('hub.model_user')::findOrFail($user);
+
+        // clearing user cache
+        return Cache::forget('hub.me.' . $user->hub_uuid);
+    }
+}
