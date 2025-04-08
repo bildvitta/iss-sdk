8e3692e6e40e90dc0c94d4dee165743d21b4bb3d
Guilherme Haynes Howe <zerossb@gmail.com>
2025-04-08 14:55:24 -0300

    Add correct Model import

--- src/Traits/User/HasCompanyLinks.php
+++ src/Traits/User/HasCompanyLinks.php
@@ -3,6 +3,7 @@
 namespace BildVitta\Hub\Traits\User;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
